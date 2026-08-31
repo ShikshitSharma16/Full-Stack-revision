8dc1524c0b86f1c179147b8622dedd065013a213
Shikshit <shikshitsharma08@gmail.com>
2026-08-31 16:14:48 +0530

    databse connected to store data in mongodb

--- express-server.js
+++ express-server.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from "express";
+import connectDatabase from "./config/database.js";
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
@@ -7,9 +9,10 @@ app.use(express.json());
 
 app.use("/users", userRouter);
 
+
 app.use((err, request, response, next)=>{
     console.log("Error:", err.message);
-
+    
     response.status(500).send("Internal Server Error");
 });
 
@@ -18,32 +21,34 @@ app.use((err, request, response, next)=>{
     console.log("Method:", request.method);
     console.log("URL:", request.url);
     next();
-});
-
-app.get("/", (request, response)=>{
-    console.log("Route executed");
-
-    response.send("Home Page");
-});*/
-
-function checkAuth(request, response, next){
-    const isAuthenticated = true;
-
-    if(!isAuthenticated){
-        return response.status(401).send("Unauthorize");
-    }
-
-    next();
-}
-
-app.get("/profile", checkAuth, (request, response)=>{
-    response.send("Welcome to profile page");
-});
-
-
-app.use((request, response)=> {
-    response.status(404).send("404 - Page not found")
-});
+    });
+    
+    app.get("/", (request, response)=>{
+        console.log("Route executed");
+        
+        response.send("Home Page");
+        });*/
+        
+        function checkAuth(request, response, next){
+            const isAuthenticated = true;
+            
+            if(!isAuthenticated){
+                return response.status(401).send("Unauthorize");
+            }
+            
+            next();
+        }
+        
+        app.get("/profile", checkAuth, (request, response)=>{
+            response.send("Welcome to profile page");
+        });
+        
+        
+        app.use((request, response)=> {
+            response.status(404).send("404 - Page not found")
+        });
+        
+connectDatabase();
 
 app.listen(3000, () => {
     console.log("Express Server listening on port 3000")
